ff60fac9e6d7e85d3c5e8f495e3beb83d66274fb
Thiago Galvez <thiagogalvezx@gmail.com>
2023-04-05 14:57:04 -0300

    parametros-URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Menu Ativo
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -9,3 +11,16 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
